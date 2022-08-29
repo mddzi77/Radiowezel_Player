de1f773af9fa588e2f85c3b1b1c3c0fc8114bfb6
mddzi77 <md.dziem@gmail.com>
2022-08-29 20:22:27 +0200

    Major structure changes, better look of window and finally working table

--- gui.py
+++ gui.py
@@ -1,89 +1,154 @@
 import PySimpleGUI as sg
 from screeninfo import get_monitors
+from random import randrange
+
+data = [[randrange(0, 100) for i in range(4)] for j in range(100)]
+data2 = [[1, 'GenYoutube.net_Mike-Posner-I-Took-A-Pill-In-Ibiza-Original_41GZVVcxQps', '22:22'],
+         [2, '01 Macklemore  Ryan Lewis - Can_t Hold Us _ft Ray Dalton__', '33:33']]
+
+print(data)
+
+''' Global default values for widgets and windows'''
+# color palette used in app
+colors = {
+    'TEXT_1': '#EFF9F9',
+    'TEXT_2': '#28405E',
+    'COLOR_1': '#448CC9',
+    'COLOR_2': '#99D0EE',
+    'COLOR_3': '#DAECFB',
+    'BGR_1': '#85AEC5',
+    'BGR_2': '#7197AD',
+    'BGR_3': '#A2CBE2',
+    'WHITE': '#FFFFFF',
+    'SNOW': '#FFFAFA',
+    'GREY': '#7B8FA2'
+        }
+# gets main screen size
+for m in get_monitors():
+    if m.is_primary:
+        screen = (m.width, m.height)
+
+
+# widgets used in app
+class Button(sg.Button):
+    def __init__(self, *args, border_width=0, font='Comic', button_color=(colors['TEXT_1'], colors['COLOR_1']),
+                 mouseover_colors=(colors['TEXT_1'], colors['COLOR_2']), **kwargs):
+
+        super().__init__(*args, border_width=border_width, font=font, button_color=button_color,
+                         mouseover_colors=mouseover_colors, **kwargs)
+
+
+class Slider(sg.Slider):
+    def __init__(self, *args, relief=sg.RELIEF_FLAT, font='Comic', text_color=colors['TEXT_1'],
+                 trough_color=colors['BGR_3'], background_color=colors['COLOR_1'], **kwargs):
+
+        super().__init__(*args, relief=relief, font=font, text_color=text_color,
+                         trough_color=trough_color, background_color=background_color, **kwargs)
+
+
+class Text1(sg.Text):
+    def __init__(self, *args, font='Comic', text_color=colors['TEXT_1'], background_color=colors['BGR_1'], **kwargs):
+
+        super().__init__(*args, font=font, text_color=text_color, background_color=background_color, **kwargs)
+
+
+class Text2(sg.Text):
+    def __init__(self, *args, font='Comic', text_color=colors['TEXT_2'], background_color=colors['SNOW'], **kwargs):
+
+        super().__init__(*args, font=font, text_color=text_color, background_color=background_color, **kwargs)
+
+
+class ProgressBar(sg.ProgressBar):
+    def __init__(self, *args,  max_value=100, size=(80, 10), orientation='horizontal',
+                 bar_color=(colors['COLOR_1'], colors['SNOW']), **kwargs):
+
+        super().__init__(*args,  max_value=max_value, size=size, orientation=orientation,
+                         bar_color=bar_color, **kwargs)
+
+
+class Frame(sg.Frame):
+    def __init__(self, *args, background_color=colors['WHITE'], font='Comic', relief=sg.RELIEF_GROOVE,
+                 title_color=colors['COLOR_1'], **kwargs):
+
+        super().__init__(*args,  background_color=background_color, font=font, relief=relief,
+                         title_color=title_color, **kwargs)
+
+
+class Table(sg.Table):
+    def __init__(self,
+                 *args,
+                 text_color=colors['TEXT_1'],
+                 background_color=colors['BGR_3'],
+                 max_col_width=100,
+                 selected_row_colors=(colors['GREY'],colors['COLOR_3']),
+                 font='Comic',
+                 header_text_color=colors['TEXT_1'],
+                 header_background_color=colors['COLOR_1'],
+                 header_relief=sg.RELIEF_FLAT,
+                 sbar_relief=sg.RELIEF_FLAT,
+                 sbar_trough_color=colors['BGR_3'],
+                 sbar_background_color=colors['COLOR_1'],
+                 sbar_arrow_color=colors['COLOR_3'],
+                 enable_events=True,
+                 auto_size_columns=False,
+                 row_colors=None,
+                 selected_row: int = None,
+                 **kwargs):
+
+        print('taaaaaaaaaaaaaaaaaaaaaaak') if row_colors else print('nieeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+
+        super().__init__(*args,
+                         text_color=text_color,
+                         background_color=background_color,
+                         max_col_width=max_col_width,
+                         selected_row_colors=selected_row_colors,
+                         font=font,
+                         header_text_color=header_text_color,
+                         header_background_color=header_background_color,
+                         header_relief=header_relief,
+                         sbar_relief=sbar_relief,
+                         sbar_trough_color=sbar_trough_color,
+                         sbar_background_color=sbar_background_color,
+                         sbar_arrow_color=sbar_arrow_color,
+                         auto_size_columns=auto_size_columns,
+                         enable_events=enable_events,
+                         row_colors=[(selected_row, colors['COLOR_1'])] if selected_row else row_colors,
+                         **kwargs)
+
+
+class DefaultWindow(sg.Window):
+    """
+    Default look of app window
+    """
+    def __init__(self, *args, layout: list, finalize=True, background_color=colors['BGR_1'], margins=(0, 0),
+                 use_custom_titlebar=True, titlebar_background_color=colors['BGR_1'], titlebar_font='Comic',
+                 titlebar_text_color=colors['TEXT_1'], **kwargs):
+
+        layout = sg.Column(
+            [[sg.Column(layout, background_color=colors['SNOW'], pad=(8, 5), expand_x=True, expand_y=True)]],
+            background_color=colors['SNOW'],
+            pad=(4, 0),
+            expand_x=True,
+            expand_y=True)
+
+        layout = [[layout]] + [[Gui.music_progressbar()]]
+
+        super().__init__(*args, layout=layout, finalize=finalize, background_color=background_color,
+                         margins=margins, use_custom_titlebar=use_custom_titlebar,
+                         titlebar_font=titlebar_font, titlebar_text_color=titlebar_text_color,
+                         titlebar_background_color=titlebar_background_color, **kwargs)
 
 
 class Gui(object):
     """
-    Gui class containing windows and widgets inherited
-    from PySimpleGui with changed default values
+    Gui class containing windows and layouts to be used in app
     """
-
-    ''' Static values to call in inner classes '''
-    # gets main screen size
-    for m in get_monitors():
-        if m.is_primary:
-            screen = (m.width, m.height)
-    # fonts and font sizes depended on sreen size
-    fonts = {
-        'MAIN': 'Comic {}'.format(round(19 / 1920 * screen[0])),
-        'LISTS': 'Comic {}'.format(round(17 / 1920 * screen[0]))
-    }
-    # color palette
-    colors = {
-            'TEXT': '#EFF9F9',
-            'COLOR_1': '#448CC9',
-            'COLOR_2': '#99D0EE',
-            'BGR_1': '#85AEC5',
-            'BGR_2': '#7197AD',
-            'BGR_3': '#DAECFB',
-            'WHITE': '#FFFFFF',
-            'SNOW': '#FFFAFA',
-            'More Grey': '#7B8FA2'
-        }
-
     def __init__(self):
         pass
 
-    ''' Widgets used in app '''
-    class Button(sg.Button):
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(
-                *args,
-                **kwargs,
-                border_width=0,
-                font='Comic',
-                button_color=(Gui.colors['TEXT'], Gui.colors['COLOR_1']),
-                mouseover_colors=(Gui.colors['TEXT'], Gui.colors['COLOR_2'])
-            )
-
-    class Slider(sg.Slider):
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(
-                *args,
-                **kwargs,
-                relief=sg.RELIEF_FLAT,
-                font='Comic',
-                text_color=Gui.colors['TEXT'],
-                trough_color=Gui.colors['BGR_3'],
-                background_color=Gui.colors['COLOR_1']
-            )
-
-    class Text(sg.Text):
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(
-                *args,
-                **kwargs,
-                font='Comic',
-                text_color=Gui.colors['TEXT'],
-                background_color=Gui.colors['BGR_1']
-            )
-
-    class ProgressBar(sg.ProgressBar):
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(
-                *args,
-                **kwargs,
-                max_value=100,
-                size=(40, 10),
-                orientation='horizontal',
-                bar_color=(Gui.colors['COLOR_1'], Gui.colors['BGR_3'])
-            )
-
-    def music_progressbar(self):
+    ''' Common elements '''
+    @staticmethod
+    def music_progressbar():
         """
         Container for all information and controls to music playback
         :return: Ready to implement layout
@@ -91,29 +156,59 @@ class Gui(object):
 
         layout = [
             [
-                Gui.Button('play', key='_play_'),
-                Gui.Button('pause', key='_pause_'),
-                Gui.Button('stop', key='_stop_'),
-                sg.Push(background_color=Gui.colors['BGR_1']),
-                Gui.Button('No volume', key='_no_volume_')
+                sg.Push(background_color=colors['BGR_1']),
+                Button('', key='_play_pause_', image_source='icons\\play.png',
+                       button_color=(colors['BGR_1'], colors['BGR_1']), image_size=(30, 30)),
+                Button('', key='_stop_', image_source='icons\\stop.png',
+                       button_color=(colors['BGR_1'], colors['BGR_1']), image_size=(30, 30)),
+                Button('', key='_mute_unmute_', image_source='icons\\volume.png',
+                       button_color=(colors['BGR_1'], colors['BGR_1']), image_size=(30, 30)),
+                sg.Push(background_color=colors['BGR_1'])
             ],
             [
-                Gui.Text('Song:'),
-                sg.Push(background_color=Gui.colors['BGR_1']),
-                Gui.Text('--:--'),
-                Gui.ProgressBar(key='_song_progress_'),
-                Gui.Text('--:--'),
+                sg.Column(
+                    [[Text1('Song:', background_color=colors['BGR_1']),
+                      sg.Push(background_color=colors['BGR_1']),
+                      Text1('--:--', background_color=colors['BGR_1']),
+                      ProgressBar(key='_song_progress_'),
+                      Text1('--:--', background_color=colors['BGR_1'])]],
+                    colors['BGR_1'],
+                    expand_x=True
+                )
             ],
             [
-                Gui.Text('Playlist:'),
-                sg.Push(background_color=Gui.colors['BGR_1']),
-                Gui.Text('--:--'),
-                Gui.ProgressBar(key='_playlist_progress_'),
-                Gui.Text('--:--'),
-            ]
+                sg.Column(
+                    [[Text1('Playlist:', background_color=colors['BGR_1']),
+                      sg.Push(background_color=colors['BGR_1']),
+                      Text1('--:--', background_color=colors['BGR_1']),
+                      ProgressBar(key='_playlist_progress_'),
+                      Text1('--:--', background_color=colors['BGR_1'])]],
+                    colors['BGR_1'],
+                    expand_x=True
+                )
+            ],
         ]
 
-        return layout
+        layout = sg.Column(layout, colors['BGR_1'], pad=(5, 5))
+
+        return sg.Column([[layout]], colors['BGR_1'], pad=(0, 0))
+
+    @staticmethod
+    def header():
+
+        layout = [[
+            Text1('asdasd', background_color=colors['BGR_1']),
+            sg.Push(background_color=colors['BGR_1']),
+            Button(
+                image_source='icons\\close.png',
+                button_color=(colors['BGR_1'], colors['BGR_1']),
+                image_subsample=23,
+                mouseover_colors='#EB2628',
+                key='_cancel_'
+            )
+        ]]
+
+        return sg.Column(layout, colors['BGR_1'], pad=(0, 0), expand_x=True)
 
     ''' Windows used in app '''
     def main_window(self):
@@ -122,19 +217,43 @@ class Gui(object):
         :return: Window element
         """
 
-        col_1 = sg.Column([[]])
-
-        col_2 = sg.Column([[]])
-
-        layout = [
-            [col_1, col_2],
-            [self.music_progressbar()]
-        ]
-
-        return sg.Window(
-            'Main window',
-            layout,
-            finalize=True,
-            background_color=Gui.colors['BGR_1']
+        col_1 = Frame(
+            'OPTION MENU',
+            [
+                [Button('Enable Autoplay', key='_autoplay_enabler_')],
+                [Button('Music Database', key='_music_db_')],
+                [Button('Playlists Editor', key='_editor_')],
+                [Button('Settings', key='_settings_')]
+            ]
         )
 
+        col_2 = Frame(
+            '',
+            [
+                [Text2('Today\'s Playlists:')],
+                [Table(
+                    data,
+                    ['Nr', 'Start Time', 'Break\'s length', 'Playlist Length'],
+                    key='_today_plalist_',
+                    col_widths=[3, 17, 17, 18],
+                    justification='c',
+                    num_rows=10,
+                    selected_row=2,
+                    hide_vertical_scroll=True
+                )],
+                [Text2('Selected Playlist:')],
+                [Table(
+                    data2,
+                    ['Nr', 'Title', 'Length'],
+                    key='_selected_playlist_',
+                    col_widths=[3, 44, 8],
+                    justification='c',
+                    num_rows=8,
+                    hide_vertical_scroll=True
+                )]
+            ],
+        )
+
+        layout = [[col_1, sg.Push(background_color=colors['SNOW']), col_2]]
+
+        return DefaultWindow('School Radio Player', layout=layout)
